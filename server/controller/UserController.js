fb1c4552a99a601acafbb96decea84425c92f5e3
Bintang Permana Putra <bintang.p06@sgbteam.id>
2021-11-12 18:06:34 +0700

    sadsad

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -27,13 +27,51 @@ exports.create = (req,res) => {
 }
 
 exports.find = (req,res) => {
-
+    userDb.find()
+        .then(user=>
+            {
+                res.send(user)
+            })
+        .catch(err=>{
+            res.status(500).send({message:err.message || "Error Ketika Melihat Data"})
+        })
 }
 
 exports.update = (req,res) => {
+    if(!req.body){
+        return res
+            .status(400)
+            .send({message:"Data Tidak Boleh Kosong!!!"})
+    }
 
+    const id = req.params.id
+    userDb.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+        .then(data=>{
+            if(!data){
+                res.status(400).send({message: 'User Tidak Ditemukan'})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({message:err.message || "Error Ketika Update Data"})
+        })
 }
 
 exports.delete = (req,res) => {
+    const id = req.params.id
 
+    userDb.findByIdAndDelete(id)
+        .then(data=>{
+            if(!data)
+            {
+                res.status(400).send({message: 'User Tidak Ditemukan'})
+            }
+            else {
+                res.send(data)
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({message:err.message || "Error Ketika Delete Data"})
+        })
 }
